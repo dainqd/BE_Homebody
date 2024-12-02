b6e8e82b690592e0a63bab4c2e8b17f48afbf2dd
dainq <97323194+dainqd@users.noreply.github.com>
2024-12-02 21:23:26 +0700

    Fix bug.

--- app/Http/Controllers/restapi/PartnerApi.php
+++ app/Http/Controllers/restapi/PartnerApi.php
@@ -3,8 +3,56 @@
 namespace App\Http\Controllers\restapi;
 
 use App\Http\Controllers\Controller;
+use App\Models\PartnerInformations;
+use DateTime;
+use Illuminate\Http\Request;
 
 class PartnerApi extends Controller
 {
-    //
+    public function getHourlyIntervals(Request $request)
+    {
+        try {
+            $partner_id = $request->input('partner_id');
+
+            $partner = PartnerInformations::where('user_id', $partner_id)->first();
+            if (!$partner) {
+                $data = returnMessage(-1, 400, '', 'Partner not found!');
+                return response($data, 400);
+            }
+
+            $time_working = $partner->time_working;
+
+            $time_working = explode('-', $time_working);
+            $startTime = $time_working[0];
+            $endTime = $time_working[1];
+
+            $intervals = [];
+
+            $start = DateTime::createFromFormat('h:ia', $startTime);
+            $end = DateTime::createFromFormat('h:ia', $endTime);
+
+            while ($start < $end) {
+                $next = clone $start;
+                $next->modify('+1 hour');
+
+                $intervals[] = $start->format('h') . ':00 ' . strtoupper($start->format('a')) . '-' . $next->format('h') . ':00 ' . strtoupper($next->format('a'));
+
+                $start = $next;
+            }
+
+            $res = [
+                'time_day' => $time_working,
+                'time_start' => $startTime,
+                'time_end' => $endTime,
+                'time_interval' => $intervals
+            ];
+
+            $data = returnMessage(1, 200, $res, 'Success');
+            return response($data, 200);
+        } catch (\Exception $exception) {
+            $data = returnMessage(-1, 400, '', $exception->getMessage());
+            return response($data, 400);
+        }
+    }
+
 }
--- app/Http/Controllers/restapi/user/CheckoutApi.php
+++ app/Http/Controllers/restapi/user/CheckoutApi.php
@@ -3,9 +3,61 @@
 namespace App\Http\Controllers\restapi\user;
 
 use App\Http\Controllers\Api;
-use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Stripe\Charge;
+use Stripe\Stripe;
+use Stripe\Token;
+use Stripe\Exception\CardException;
+use Stripe\Exception\ApiErrorException;
 
 class CheckoutApi extends Api
 {
-    //
+    public function __construct()
+    {
+        Stripe::setApiKey(env('STRIPE_SECRET'));
+    }
+
+
+    public function processStripe(Request $request)
+    {
+        try {
+            $cardDetails = [
+                'number' => $request->input('card_number'),
+                'exp_month' => $request->input('exp_month'),
+                'exp_year' => $request->input('exp_year'),
+                'cvc' => $request->input('cvc'),
+            ];
+
+            $token = Token::create(['card' => $cardDetails]);
+            $data = returnMessage(1, 200, $token, 'Token created successfully!');
+            return response($data, 200);
+        } catch (\Exception $exception) {
+            $data = returnMessage(-1, 400, '', $exception->getMessage());
+            return response($data, 400);
+        }
+    }
+
+    public function handleCheckout(Request $request)
+    {
+        try {
+            $charge = Charge::create([
+                'amount' => $request->input('amount') * 100,
+                'currency' => 'usd',
+                'source' => $request->input('stripeToken'),
+                'description' => $request->input('description') ?? 'Thanh toán đơn hàng',
+            ]);
+
+            $data = returnMessage(1, 200, $charge, 'Thanh toán thành công!');
+            return response($data, 200);
+        } catch (CardException $e) {
+            $data = returnMessage(-1, 402, '', 'Thanh toán thất bại: ' . $e->getMessage());
+            return response($data, 402);
+        } catch (ApiErrorException $e) {
+            $data = returnMessage(-1, 500, '', 'Stripe API error: ' . $e->getMessage());
+            return response($data, 500);
+        } catch (\Exception $e) {
+            $data = returnMessage(-1, 400, '', 'Lỗi không xác định: ' . $e->getMessage());
+            return response($data, 400);
+        }
+    }
 }
