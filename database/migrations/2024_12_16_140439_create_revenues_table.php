2bcb2a2e70511b626d9aab4ac1d1c23b2ce45729
dainqd <ngodaix5tp@gmail.com>
2024-12-16 10:15:46 +0700

    Admin + partner update booking.

---
+++ database/migrations/2024_12_16_140439_create_revenues_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('revenues', function (Blueprint $table) {
+            $table->id();
+
+            $table->decimal('total', 10, 2);
+
+            $table->string('date');
+            $table->string('month');
+            $table->string('year');
+
+            $table->unsignedBigInteger('booking_id');
+            $table->foreign('booking_id')->references('id')->on('bookings')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('revenues');
+    }
+};
