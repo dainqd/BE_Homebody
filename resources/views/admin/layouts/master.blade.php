daa81b20e186efb2966a25ee28597daff0272e4c
dainqd <ngodaix5tp@gmail.com>
2024-12-26 08:43:21 +0700

    View + update sidebar.

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -6,18 +6,37 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
     <title>@yield('title')</title>
-    <meta content="" name="description">
-    <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="{{ asset('admin/img/favicon.png') }}" rel="icon">
-    <link href="{{ asset('admin/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+    <link href="{{ setting() ? setting()->favicon : asset('admin/img/favicon.png') }}" rel="icon">
+    <link href="{{ setting() ? setting()->favicon : asset('admin/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
+    <!-- Meta tag seo -->
+    <meta name="description" content="{{ setting() ? setting()->meta_tag : '' }}">
+    <meta name="keywords" content="{{ setting() ? setting()->meta_keyword : '' }}">
+    <meta name="robots" content="index, follow">
+
+    <!-- Open Graph (Facebook, LinkedIn) -->
+    <meta property="og:title" content="{{ setting() ? setting()->og_title : '' }}">
+    <meta property="og:description" content="{{ setting() ? setting()->og_des : '' }}">
+    <meta property="og:image" content="{{ setting() ? setting()->og_img : '' }}">
+    <meta property="og:url" content="{{ setting() ? setting()->og_url : '' }}">
+    <meta property="og:type" content="{{ setting() ? setting()->og_site : '' }}">
+
+    <!-- Twitter Card -->
+    <meta name="twitter:card" content="{{ setting() ? setting()->og_site : '' }}">
+    <meta name="twitter:title" content="{{ setting() ? setting()->og_title : '' }}">
+    <meta name="twitter:description" content="{{ setting() ? setting()->og_des : '' }}">
+    <meta name="twitter:image" content="{{ setting() ? setting()->og_img : '' }}">
+
+    <!-- Meta Author -->
+    <meta name="author" content="{{ setting() ? setting()->owner_name : '' }}">
 
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
     <link
-        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-        rel="stylesheet">
+            href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+            rel="stylesheet">
 
     <!-- Vendor CSS Files -->
     <link href="{{ asset('admin/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
@@ -97,7 +116,7 @@
 <!-- End Footer -->
 
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-        class="bi bi-arrow-up-short"></i></a>
+            class="bi bi-arrow-up-short"></i></a>
 
 <!-- Vendor JS Files -->
 <script src="{{ asset('admin/vendor/apexcharts/apexcharts.min.js') }}"></script>
--- resources/views/error/layouts/master.blade.php
+++ resources/views/error/layouts/master.blade.php
@@ -6,12 +6,31 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
     <title>@yield('title')</title>
-    <meta content="" name="description">
-    <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="{{ asset('admin/img/favicon.png') }}" rel="icon">
-    <link href="{{ asset('admin/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+    <link href="{{ setting() ? setting()->favicon : asset('admin/img/favicon.png') }}" rel="icon">
+    <link href="{{ setting() ? setting()->favicon : asset('admin/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
+    <!-- Meta tag seo -->
+    <meta name="description" content="{{ setting() ? setting()->meta_tag : '' }}">
+    <meta name="keywords" content="{{ setting() ? setting()->meta_keyword : '' }}">
+    <meta name="robots" content="index, follow">
+
+    <!-- Open Graph (Facebook, LinkedIn) -->
+    <meta property="og:title" content="{{ setting() ? setting()->og_title : '' }}">
+    <meta property="og:description" content="{{ setting() ? setting()->og_des : '' }}">
+    <meta property="og:image" content="{{ setting() ? setting()->og_img : '' }}">
+    <meta property="og:url" content="{{ setting() ? setting()->og_url : '' }}">
+    <meta property="og:type" content="{{ setting() ? setting()->og_site : '' }}">
+
+    <!-- Twitter Card -->
+    <meta name="twitter:card" content="{{ setting() ? setting()->og_site : '' }}">
+    <meta name="twitter:title" content="{{ setting() ? setting()->og_title : '' }}">
+    <meta name="twitter:description" content="{{ setting() ? setting()->og_des : '' }}">
+    <meta name="twitter:image" content="{{ setting() ? setting()->og_img : '' }}">
+
+    <!-- Meta Author -->
+    <meta name="author" content="{{ setting() ? setting()->owner_name : '' }}">
 
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
